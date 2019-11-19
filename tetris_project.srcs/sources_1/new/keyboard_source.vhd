9b953d8a9dc6923ee61c9fad8451af8df98d359f
ygunarso <sgunarso21@gmail.com>
2019-11-19 13:30:03 -0500

    Initial commit

---
+++ tetris_project.srcs/sources_1/new/keyboard_source.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/31/2019 01:22:03 PM
+-- Design Name: 
+-- Module Name: keyboard_source - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity keyboard_source is
+--  Port ( );
+end keyboard_source;
+
+architecture Behavioral of keyboard_source is
+
+begin
+
+
+end Behavioral;
---
+++ tetris_project.srcs/sources_1/new/tetris_data.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/31/2019 12:50:06 PM
+-- Design Name: 
+-- Module Name: tetris_data - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tetris_data is
+--  Port ( );
+end tetris_data;
+
+architecture Behavioral of tetris_data is
+
+begin
+
+
+end Behavioral;
